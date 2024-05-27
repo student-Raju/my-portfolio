df10d81d6b489f7c5f183646d2806ae985a4013a
student-Raju <rajgopalmahata@gmail.com>
2024-05-27 23:40:50 +0530

    portfolio

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
-import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
-import { title } from 'process'
-import { gridItems } from '@/data'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
+import { gridItems } from '@/data';
 
 export const Grid = () => {
   return (
     <section id="about">
-     <BentoGrid>
-        {gridItems.map((item)=>(
-            <BentoGridItem 
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            description={item.description}
-            />
-        ))}
-
-     </BentoGrid>
+      <BentoGrid>
+        {gridItems.map
+       (({id,title,description,className,imgClassName,titleClassName,spareImg,img})=>(
+       <BentoGridItem
+       id={id}
+       key={id}
+       title={title}
+       description={description}
+       className={className}
+       img={img}
+       imgClassName={imgClassName}
+       titleClassName={titleClassName}
+       spareImg={spareImg}
+       />
+       )) }
+      </BentoGrid>
     </section>
   )
 }
+export default Grid;
