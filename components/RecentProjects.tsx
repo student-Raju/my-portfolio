fc8b8ccb5d33e9ecc33705e8b538ef71b6ebae7c
student-Raju <rajgopalmahata@gmail.com>
2024-05-29 08:50:40 +0530

    portfolio

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,7 +7,7 @@ import { FaLocationArrow } from 'react-icons/fa'
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
         <h1 className="heading">
           A small selection of {''}
           <span className='text-purple'>recent projects</span>
@@ -16,7 +16,7 @@ const RecentProjects = () => {
             {projects.map(({id, title, des,img,  iconLists,link, })=>(
                 
                 <div key={id} className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5]  flex items-center justify-center sm:w-[570px] w-[80vw]">
-                    <PinContainer title={title} href={link}>
+                    <PinContainer title={link} href={link}>
                      <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh]  mb-10'>
                         <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                             <img src="/bg.png" alt="bg-img"/>
@@ -35,8 +35,9 @@ const RecentProjects = () => {
                             <div className="flex items-center">
                                 {iconLists.map((icon,index)=>(
                                         <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg-w-10 lg:h-10 w-8 h-8 flex justify-center items-center'
-                                        style={{transform:`translateX(-${5*index*2}px)`
-                                        }}>
+                                        style={{
+                                            transform: `translateX(-${5 * index + 2}px)`,
+                                          }}>
                                             <img src={icon} alt={icon} className="p-2"/>
                                         </div>
                                 ))}
