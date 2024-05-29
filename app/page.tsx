bb48e344e77459cbfa2f6ac67d205d3e7cc88510
student-Raju <rajgopalmahata@gmail.com>
2024-05-29 17:31:09 +0530

    portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
@@ -21,6 +22,7 @@ export default function Home() {
        <RecentProjects/>
        <Clients/>
        <Experience/>
+       <Approach/>
        </div>
     </main>
   );
